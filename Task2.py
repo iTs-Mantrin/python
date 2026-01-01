78e7bb3c7e600029d69344a7813a858c0ebf7a3c
Manish Kumar <manish.rajput62647457@gmail.com>
2026-01-01 20:35:37 +0530

    Added new feature

---
+++ Task2.py
@@ -0,0 +1,6 @@
+first_name = input("Please enter first name:")
+last_name = input("Please enter last name:")
+
+full_name = first_name +" "+last_name
+
+print("Hello,",full_name+"!","Welcome to the Python programm.")
